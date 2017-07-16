2bd2c03409a6cb8b9516efa79c2fa27c016c99a6
VILLERS Mickaël <mickael.villers@epitech.eu>
2017-07-16 18:48:04 +0200

    page 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <br><br>
+    <div id="tf-team">
+        <div class="container">
+            <div class="section-title center text-center">
+                <h2>404<strong> Page Not Found</strong></h2>
+                <div class="line">
+                    <hr>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <script>
+        $('.navbar-default').addClass('on');
+    </script>
+@endsection
