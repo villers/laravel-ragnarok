d2ea31996cf8fe551ba62fbc1e4958a1cd6bcf5f
VILLERS Mickaël <mickael.villers@epitech.eu>
2018-03-25 18:17:24 +0200

    fix emblem

--- app/Lib/Ragnarok/Emblem.php
+++ app/Lib/Ragnarok/Emblem.php
@@ -10,13 +10,36 @@ class Emblem
         $info = unpack("Vsize/Vwidth/Vheight/vplanes/vbits/Vcompression/Vimagesize/Vxres/Vyres/Vncolor/Vimportant", substr($im, 14, 40));
 
         $offset = $header['offset'];
+        $type = $header['type'];
         $width = $info['width'];
         $height = $info['height'];
         $bits = $info['bits'];
 
+        if ($type != 0x4D42) {
+            return false;
+        }
+
         $imres = imagecreatetruecolor($width, $height);
         imagealphablending($imres, false);
         imagesavealpha($imres, true);
+
+        $pal = [];
+        $palette_size = $offset - 54;
+        if ($palette_size) {
+            $palette = substr($im, 54, $palette_size);
+            $j = 0;
+            $n = 0;
+            while ($j < $palette_size) {
+                $b = ord($palette{$j++});
+                $g = ord($palette{$j++});
+                $r = ord($palette{$j++});
+                $a = ord($palette{$j++});
+                if (($r & 0xf8 == 0xf8) && ($g == 0) && ($b & 0xf8 == 0xf8)) {
+                    $a = 127;
+                } // alpha = 255 on 0xFF00FF
+                $pal[$n++] = imagecolorallocatealpha($imres, $r, $g, $b, $a);
+            }
+        }
 
         $scan_line_size = (($bits * $width) + 7) >> 3;
         $scan_line_align = ($scan_line_size & 0x03) ? 4 - ($scan_line_size & 0x03): 0;
@@ -37,8 +60,47 @@ class Emblem
                     $col = imagecolorallocatealpha($imres, $r, $g, $b, $a);
                     imagesetpixel($imres, $n++, $i, $col);
                 }
+            } elseif ($bits == 8) {
+                $j = 0;
+                while ($j < $scan_line_size) {
+                    $col = $pal[ord($scan_line{$j++})];
+                    imagesetpixel($imres, $j-1, $i, $col);
+                }
+            } elseif ($bits == 4) {
+                $j = 0;
+                $n = 0;
+                while ($j < $scan_line_size) {
+                    $byte = ord($scan_line{$j++});
+                    $p1 = $byte >> 4;
+                    $p2 = $byte & 0x0F;
+                    imagesetpixel($imres, $n++, $i, $pal[$p1]);
+                    imagesetpixel($imres, $n++, $i, $pal[$p2]);
+                }
+            } elseif ($bits == 1) {
+                $j = 0;
+                $n = 0;
+                while ($j < $scan_line_size) {
+                    $byte = ord($scan_line{$j++});
+                    $p1 = (int) (($byte & 0x80) != 0);
+                    $p2 = (int) (($byte & 0x40) != 0);
+                    $p3 = (int) (($byte & 0x20) != 0);
+                    $p4 = (int) (($byte & 0x10) != 0);
+                    $p5 = (int) (($byte & 0x08) != 0);
+                    $p6 = (int) (($byte & 0x04) != 0);
+                    $p7 = (int) (($byte & 0x02) != 0);
+                    $p8 = (int) (($byte & 0x01) != 0);
+                    imagesetpixel($imres, $n++, $i, $pal[$p1]);
+                    imagesetpixel($imres, $n++, $i, $pal[$p2]);
+                    imagesetpixel($imres, $n++, $i, $pal[$p3]);
+                    imagesetpixel($imres, $n++, $i, $pal[$p4]);
+                    imagesetpixel($imres, $n++, $i, $pal[$p5]);
+                    imagesetpixel($imres, $n++, $i, $pal[$p6]);
+                    imagesetpixel($imres, $n++, $i, $pal[$p7]);
+                    imagesetpixel($imres, $n++, $i, $pal[$p8]);
+                }
             }
         }
+
         return $imres;
     }
 }
